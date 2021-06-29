bdd97fa3b8c273b6b904cf6df8538da2e02c3395
Shaun Wassell <shaun@trove.ai>
2021-06-28 21:11:05 -0400

    Finished code for the course

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Welcome to {{ siteName }}!</h1>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    data() {
+        return {
+            siteName: 'Learn Vue LiveLessons',
+        };
+    }
+}
+</script>
+
+<style>
+h1 {
+    color: red;
+}
+</style>
